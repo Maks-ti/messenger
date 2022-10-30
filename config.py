7daae076a5046c1c1bda45f643973003951ecce8
Maks-ti <maxtitkov3@gmail.com>
2022-10-31 01:31:24 +0300

    conf

--- config.py
+++ config.py
@@ -8,5 +8,10 @@ import os
 класс конфигурации приложения
 '''
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-my-key'
     UPLOAD_FOLDER = 'C:/Users/maxti/PycharmProjects/messenger/app/static/images/'
+    DB_NAME = 'messenger'
+    DB_USER = 'maxti'
+    USER_PASSWORD = 'psql_password'
+    DB_HOST = 'localhost'
+    DB_PORT = '5432'
